25231c406a8643afe85cb933112260541d16ca1a
VN\ducdoan <lovecherub9@gmail.com>
2018-09-04 13:28:27 +0700

    Add input params

--- www/plugin.js
+++ www/plugin.js
@@ -4,7 +4,16 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'GooglePay';
 
 var GooglePay = {
-  saveToGooglePay: function(successCallback, errorCallback, args) {
+  saveToGooglePay: function(successCallback, errorCallback, issuerId, loyaltyClassId, loyaltyObjectId, accountId, accountName, issuerName, programName) {
+    var args = {};
+    args.issuerId = issuerId;
+    args.loyaltyClassId = loyaltyClassId;
+    args.loyaltyObjectId = loyaltyObjectId;
+    args.accountId = accountId;
+    args.accountName = accountName;
+    args.issuerName = issuerName;
+    args.programName = programName;
+    
     exec(successCallback, errorCallback, PLUGIN_NAME, 'saveToGooglePay', [args]);
   },
 };
